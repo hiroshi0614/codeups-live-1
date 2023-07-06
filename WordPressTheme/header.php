d197e7662028fc9e02b64e24aa76b4712468842d
hiroshi ito <max0614hiroshi@yahoo.co.jp>
2023-07-07 00:24:22 +0900

    0707

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -5,10 +5,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;600&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
-  <header class="">
-    <p class="p-test">aaaa</p>
+  <header class="l-header p-header">
+    <div class="p-header__inner">
+      <div class="p-header__logo">
+        <img class="c-logo" src="<?php echo get_template_directory_uri() ?>/assets/img/common/logo.svg" alt="タイトルロゴ">
+      </div>
+      <div class="p-header__menu c-hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  <p>ああああ</p>
   </header>
